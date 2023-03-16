970bbc9dfc8d47e7dcc63638266f73fb562bb08e
AlexSoul <fantomfbt@gmail.com>
2023-03-16 14:38:42 +0500

    Initial commit of the DataTest framework
    DataNest, light as feather, fast as jet. Easy sql framework for python

---
+++ README.md
@@ -0,0 +1,79 @@
+# DataNest
+
+To use DataNest frist you need to import Datanest module.
+
+i.g 
+
+from datanest import DataNest
+
+Then create configuration file inside %your project's root dicrectory%/conf with .json extension.
+
+For example, MyProject/conf/main.json
+
+{
+    "database":{
+        "type":"sqlite3",
+        "connection":{
+            "database":"test.db",
+            "username":"test",
+            "password":"pass"
+        }
+    },
+    "datasets_path":"mydatasets",
+    "datasets": ["auth","reports"]
+}
+
+
+Where database.type stands for database driver (e.g. sqllite3,cx_Oracle,psycopg2,mariadb,etc...) !Don't forget to install the database module, but you don't have to import it into your project!
+"database.connection" stands for connection arguments (e.g. TNS, authorization credentials, etc...)
+
+"datasets_path" declares the path to your datasets config files from your project's root directory. For example, "mydatasets" means that DataNest will look up datasets in MyProject/mydatasets/
+"datasets": declares a list of dataset configuration files, which must be located inside your "datasets_path" directory, and have .json extension. (i.g. MyProject/mydatasets/reports.json)
+
+The dataset file must have the following json structure:
+{
+    query_name1:"sql query text $(replacable_parameter)s",
+    query_name2:"sql query text $(replacable_parameter)s"
+}
+
+Simple dataset for example:
+
+{
+    "get_template":"select * from dn_template where name = '%(name)s'",
+    "get_templates":"select * from dn_template"
+}
+
+
+____________________________________________________
+Operators
+
+To initialize the DataNest module, you must initialize an object.
+
+datanest = DataNest("main") # In this example, "main" goes for your configuration file.
+
+datanest.open() # Opens database connection
+datanest.close() # Closes database connection
+datanest.query(query_name:str, arguments:dict) # executes sql query, safely replacing all %(param)s with arguments
+datanest.commit() # Commits transaction to database
+datanest.execute() # Opens a new connection, executes query, and immediately commits and closes connection
+
+____________________________________________________
+
+Simple example of using DataNest in code using dataset above:
+
+
+
+from datanest Import DataNest
+
+datanest = DataNest('main')
+
+datanest.open()
+template = datanest.query('reports.get_template',{'name':'template_1'})
+print (template)
+datanest.close()
+
+
+
+##############
+
+Yup that's simple =)
---
+++ datanest.py
@@ -0,0 +1,135 @@
+class DataNest:
+
+    import json
+    import importlib
+    from os import getcwd as current_path
+
+    #Attribute constants
+    class NestAttributes:
+        database = "database"
+        datasets = "datasets"
+        datasets_path = "datasets_path"
+        connection = "connection"
+        name = "name"
+        type = "type"
+
+    #Simple constants
+    json_sufix = ".json"
+    dot = "."  
+    slash = "/"   
+     
+    #Semi static variables 
+    __version__ = 1.003
+    __cur_path = current_path()+slash
+    __conf_path = f"{__cur_path}conf/"
+    
+
+    #Local variables
+    __config = None
+    __datasets = None
+    __db_module = None
+    __db_type : str
+    __db_attr = None
+    __connection = None
+    __cursor = None
+    __is_connection_opened = False
+
+    # Simple file load to make code more readable
+    def _load_file(self,dataset):
+        with open (dataset) as _file:
+            return self.json.loads (_file.read())
+    
+    # Iterating thorugh all queries inside sigle dataset, fills up prepaired dataset. key transforms to dataset.query format.
+    def __serialize_sub_dataset(self,dataset_name,queries):
+        for query, query_value in queries.items():
+            self.__datasets[dataset_name+self.dot+query]=query_value
+
+    # Initiating dataset list        
+    def __serialize_dataset(self,dataset_name,queries):
+        # Fullfille datasets list if not definied previously
+        if self.__datasets is None:
+            self.__datasets = {dataset_name+self.dot+query: query_value for query, query_value in queries.items()}
+        else: 
+            self.__serialize_sub_dataset(dataset_name,queries)
+
+    # Loading datasets and it's queries from dataset file
+    def __load_datasets(self):
+        try:
+            datasets_path = self.__cur_path+self.__config[self.NestAttributes.datasets_path]+self.slash
+        except (KeyError):
+            print ("Warning! Unable to determine datasets path! Add argument:",self.NestAttributes.datasets_path)
+            datasets_path = self.__cur_path
+       
+        try:
+            # Iterating and loading datasets from files
+            for dataset_name in self.__config[self.NestAttributes.datasets]:
+                queries = self._load_file(datasets_path+dataset_name+self.json_sufix)
+                self.__serialize_dataset(dataset_name,queries)
+        except (FileNotFoundError): 
+            print ("Warning! Unable to locate dataset file:",datasets_path+dataset_name+self.json_sufix, "Dataset was not loaded!")
+
+    # Open connection to db
+    def open(self):   
+        if (not self.__is_connection_opened):
+            self.__connection = self.__db_module.connect(**self.__db_attr)
+            self.__cursor = self.__connection.cursor()
+            self.__is_connection_opened=True
+
+    # Commit changes
+    def commit(self):
+        try:
+            self.__connection.commit()    
+        except(AttributeError):
+            print ("Error! Open connection first!")
+
+    # Close connection
+    def close(self):
+        try:
+            self.__cursor.close()
+            self.__connection.close()
+            self.__is_connection_opened=False
+        except(AttributeError):
+            print ("Error! Open connection first!")
+
+    # Wrapper that makes function transactional, (Opens connection, executes function, commits changes, closes connection)
+    def _transactional(query_func):
+        def wrapper (*args, **kwargs):
+            args[0].open()
+            result = query_func(*args, **kwargs)
+            args[0].commit()
+            args[0].close()
+            return result
+        return wrapper
+    
+    # Executes simple query, safely inserts parameters
+    def query (self, query_name:str, params: dict=None):
+        try:
+            # When params not defined, executing without formating
+            if params:
+                result = self.__cursor.execute(self.__datasets[query_name]%params)
+            else:
+                result = self.__cursor.execute(self.__datasets[query_name])
+            return result
+        except(KeyError):
+            print ("Error! Wrong dataset or query name:",query_name)
+        except(AttributeError):
+            print ("Error! Open connection first!")
+    
+    # Opens a new connection, executes query, and immediately commits and closes connection
+    @_transactional
+    def execute (self, query_name:str, params: dict=None):
+        return self.query(query_name,params)
+
+    # Loading main configuration
+    def __init__(self, config):
+        try:
+            self.__conf_path+=config+self.json_sufix
+            self.__config = self._load_file(self.__conf_path)
+            self.__db_type = self.__config[self.NestAttributes.database][self.NestAttributes.type]
+            self.__db_attr = self.__config[self.NestAttributes.database][self.NestAttributes.connection]
+            self.__db_module = self.importlib.import_module(self.__db_type)
+        except (FileNotFoundError):
+            print ("Critical error! Unable to locate conf file:",self.__conf_path)
+        except (ModuleNotFoundError):
+            print ("Critical error! Wrong database module:",self.__config[self.NestAttributes.database])
+        self.__load_datasets()         
